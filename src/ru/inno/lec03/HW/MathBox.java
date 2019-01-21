b92c104cb16e2ec3a6c66bfd9520d65d8d3c1245
Ilya Kruglov <ily-spb@yandex.ru>
2019-01-21 15:59:32 +0300

    HomeWork lec03 refactoring

--- src/ru/inno/lec03/HW/MathBox.java
+++ src/ru/inno/lec03/HW/MathBox.java
@@ -1,98 +1,73 @@
 package ru.inno.lec03.HW;
 
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.HashSet;
 import java.util.TreeSet;
 
-public class MathBox<T extends Number> {
+/**
+ * Класс для хранения коллекции в упоря виде, получения суммы и деления
+ *
+ * @param <T>
+ */
 
-    private TreeSet<T> in;
-    final private int id;
+public class MathBox<T extends Number> extends ObjectBox{
 
-    public int getId() {
-        return id;
+
+    public static void main(String[] args) {
+
+        System.out.println("-------пример с Integer----------");
+        MathBox<Integer> mb = new MathBox<Integer>(new Integer[]{9,4,6,8,5,3,12,7,18,15,13});
+        System.out.println(mb);
+        System.out.println("summ : " + mb.summator());
+
+        TreeSet<Double> resDiv = mb.splitter( 5d);
+        System.out.println(resDiv);
+        mb.deleteObject(12);
+        System.out.println(mb);
+
+        System.out.println("------------пример с Float--------------");
+        MathBox<Float> mb1 = new MathBox<Float>(new Float[]{9f,4f,6f,8f,5f,3f,12f,7f,18f,15f,13f});
+        System.out.println(mb1);
+        System.out.println("summ : " + mb1.summator());
+
+        TreeSet<Double> resDiv1 = mb1.splitter( 5d);
+        System.out.println(resDiv1);
     }
 
-
+    /**
+     *
+     * @param arr
+     */
     public MathBox(T[] arr) {
-        in = new TreeSet<T>(Arrays.asList(arr));
-
-        id = (int)Math.random()*2147483647;
+        super(arr);
     }
 
-    @Override
-    public int hashCode() {
-        return id;
-    }
 
-    @Override
-    public boolean equals(Object obj) {
-
-        if (obj == this) return true;
-
-        if (obj == null || obj.getClass() != this.getClass()) return false;
-
-        return getId() == ((MathBox)obj).getId();
-    }
-
-    @Override
-    public String toString() {
-        return "MathBox{" +
-                "in=" + in +
-                '}';
-    }
-
-    public void print(){
-        for (T current : in) {
-            System.out.println(current);
-        }
-    }
-
+    /**
+     *
+     * @return
+     */
     public long summator(){
 
         long sum = 0l;
-        for (T current : in) {
 
+        TreeSet<T> in = getIn();
+        for (T current : in) {
             sum += current.longValue();
         }
         return sum;
     }
 
+
+    /**
+     *
+     * @param div
+     * @return
+     */
     public TreeSet<Double> splitter(Double div){
         TreeSet<Double> out  = new TreeSet<Double>();
+        TreeSet<T> in = getIn();
         for (T current : in) {
             out.add(current.doubleValue()/div);
         }
         return out;
-    }
-
-    public void delete(T val){
-        in.remove(val);
-    }
-
-
-
-
-    public static void main(String[] args) {
-
-        MathBox<Integer> mb = new MathBox<Integer>(new Integer[]{9,4,6,8,5,3,12,7,18,15,13});
-        mb.print();
-        System.out.println("summ : " + mb.summator());
-        TreeSet<Double> resDiv = mb.splitter( 5d);
-        for (Double aDouble : resDiv) {
-            System.out.println(" res div : " + aDouble);
-        }
-        mb.delete(99);
-        mb.print();
-
-        MathBox<Long> mb1 = new MathBox<Long>(new Long[]{9l,4l,6l,8l,5l,3l,12l,7l,18l,15l,13l});
-        mb1.print();
-        System.out.println("summ : " + mb1.summator());
-        TreeSet<Double> resDiv1 = mb1.splitter( 5d);
-        for (Double aDouble : resDiv1) {
-            System.out.println(" res div : " + aDouble);
-        }
-
     }
 }
--- src/ru/inno/lec03/HW/ObjectBox.java
+++ src/ru/inno/lec03/HW/ObjectBox.java
@@ -1,6 +1,118 @@
 package ru.inno.lec03.HW;
 
-import java.util.LinkedList;
+import java.util.Arrays;
+import java.util.TreeSet;
+import java.util.Random;
 
-public class ObjectBox {
+/**
+ * Класс для хранения массива в упорядоченом виде и различных манипуляций с ним
+ * @param <T>
+ */
+
+
+public class ObjectBox<T> {
+
+    private TreeSet<T> in;
+    final private int id;
+
+
+    public static void main(String[] args) {
+
+
+        System.out.println("-------пример с String----------");
+        ObjectBox<String> mb = new ObjectBox<String>(new String[]{"f","e","asdfewg","qomciwi"});
+        System.out.println(mb);
+
+        mb.deleteObject("e");
+        System.out.println(mb);
+
+        System.out.println("----------------пример с Long-------------");
+        ObjectBox<Long> mb1 = new ObjectBox<Long>(new Long[]{9l,4l,6l,8l,5l,3l,12l,7l,18l,15l,13l});
+        System.out.println(mb1);
+        }
+
+
+    public TreeSet<T> getIn() {
+        return in;
+    }
+
+    /**
+     *
+     * @param arr
+     */
+    public ObjectBox(T[] arr) {
+        in = new TreeSet<T>(Arrays.asList(arr));
+
+        Random random = new Random();
+        id = random.nextInt();
+    }
+
+
+    /**
+     *
+     * @return
+     */
+    @Override
+    public String toString() {
+        return "Box content{"+ in + '}';
+    }
+
+    /**
+     *
+     * @return
+     */
+    public String dump() {
+        return "dump{"+ in +'}';
+    }
+
+    /**
+     *
+     * @param obj
+     * @return
+     */
+    @Override
+    public boolean equals(Object obj) {
+
+        if (obj == this) return true;
+
+        if (obj == null || obj.getClass() != this.getClass()) return false;
+
+        return getId() == ((MathBox)obj).getId();
+    }
+
+
+    /**
+     *
+     * @return
+     */
+    @Override
+    public int hashCode() {
+        return id;
+    }
+
+
+    /**
+     *
+     * @return
+     */
+    public int getId() {
+        return id;
+    }
+
+
+    /**
+     *
+     * @param val
+     */
+    public void addObject(T val){
+        in.add(val);
+    }
+
+    /**
+     *
+     * @param val
+     */
+    public void deleteObject(T val){
+        in.remove(val);
+    }
 }
