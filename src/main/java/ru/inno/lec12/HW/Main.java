831bb662fcb5812fc4cdb23c868be75c453e1ba7
Ilya Kruglov <ily-spb@yandex.ru>
2019-02-02 20:54:05 +0300

    Home work 12 DAO bugfix

--- src/main/java/ru/inno/lec12/HW/Main.java
+++ src/main/java/ru/inno/lec12/HW/Main.java
@@ -10,21 +10,23 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class Main {
-    public static void main(String[] args) throws ClassNotFoundException, SQLException {
+    public static void main(String[] args) {
         String url = "jdbc:postgresql://localhost:5432/ilya";
 
-        Connection connection = DriverManager.getConnection(url);
-        System.out.println(connection);
+        try (Connection connection = DriverManager.getConnection(url)) {
+            System.out.println(connection);
 
-        PersonDAO dao = new PersonDAOImpl(connection);
+            PersonDAO dao = new PersonDAOImpl(connection);
 
-        Person person = new Person("John Smith", System.currentTimeMillis());
-        dao.createPerson(person);
-        System.out.println(person);
+            Person person = new Person("John Smith", System.currentTimeMillis());
+            dao.createPerson(person);
+            System.out.println(person);
 
-        dao.joinToCourse(person, new Subject(1, "Phisic"), new Subject(2, "Literature"));
+            dao.joinToCourse(person, new Subject(1, "Phisic"), new Subject(2, "Literature"));
 
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
 
-        // в конце не забыть закрыть подключение
     }
 }
